48060eee2be401e7e75cf86702e649e3ec2c87e0
Ivan Milov <ivan.oyra.milov@gmail.com>
2015-10-17 21:16:03 +0300

    install script improvement

--- install.sh
+++ install.sh
@@ -1,18 +1,68 @@
 #!/bin/bash
 
-ln -s $( pwd )/bin/reminder.sh /$HOME/_
+EXCLUDE="install.sh bin"
+ARGC=$#
 
-for cf in $( ls )
+CF_LIST=$( ls )
+DBG=
+
+
+# CF_LIST="test.txt"
+# DBG=qwe
+
+function inst()
+{
+    cf=$1    ; shift 
+    ln=$1    ; shift 
+
+    if [ $DBG ]
+    then
+        echo "[ -e $ln ] && mv $ln $ln.bak"
+        echo "ln -s $cf $ln"
+    else
+        [ -e $ln ] && mv $ln $ln.bak
+        ln -s $cf $ln
+    fi
+}
+
+function uninst()
+{
+    ln=$1    ; shift 
+
+    if [ $DBG ]
+    then
+        echo "unlink $ln"
+        echo "[ -e $ln.bak ] && mv $ln.bak $ln"
+    else
+        unlink $ln
+        [ -e $ln.bak ] && mv $ln.bak $ln
+    fi
+}
+
+function switch()
+{
+    cf=$( pwd )/$1  ; shift
+    ln=/$HOME/$1    ; shift 
+
+    if [[ $ARGC == 0 ]]
+    then
+        inst $cf $ln
+    else
+        uninst $ln
+    fi
+}
+
+for cf in $CF_LIST
 do
-    if [[ $cf == "install.sh" ]]
+    if [[ $EXCLUDE =~ (^| )$cf($| ) ]]
     then
 	continue
     fi
 
-    ln=/$HOME/.$cf
-    [ -e $ln ] && mv $ln $ln.bak
-    ln -s $( pwd )/$cf $ln
+    ln=.$cf
+    switch $cf $ln
 done
 
+switch bin/reminder.sh _
 
 	  
